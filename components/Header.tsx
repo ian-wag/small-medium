9863ffee39a1e5076ebd0a39a93b630756160941
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2022-01-20 15:49:04 -0600

    minor style change

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
     <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
         <Link href="/">
-          <img className="w-48 object-contain cursor-pointer" src="/logo.png" alt="logo" />
+          <img className="w-32 object-contain cursor-pointer" src="/logo.png" alt="logo" />
         </Link>
         <div className="hidden md:inline-flex items-center space-x-5">
           <h3>About</h3>
