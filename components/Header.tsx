226912e6b39b0df46620fb94d92eae78a44f11ad
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2022-01-21 16:41:13 -0600

    added cursor-pointer to buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,14 +8,16 @@ function Header() {
           <img className="w-32 object-contain cursor-pointer" src="/logo.png" alt="logo" />
         </Link>
         <div className="hidden md:inline-flex items-center space-x-5">
-          <h3>About</h3>
-          <h3>Contact</h3>
-          <h3 className="text-white bg-green-600 p-4 py-1 rounded-full">Follow</h3>
+          <h3 className="cursor-pointer">About</h3>
+          <h3 className="cursor-pointer">Contact</h3>
+          <h3 className="text-white bg-green-600 p-4 py-1 rounded-full cursor-pointer">Follow</h3>
         </div>
       </div>
-      <div className="flex items-center space-x-5 text-green-600">
+      <div className="flex items-center space-x-5 text-green-600 cursor-pointer">
         <h3>Sign In</h3>
-        <h3 className="border border-green-600 px-4 py-1 rounded-full">Get Started</h3>
+        <h3 className="border border-green-600 px-4 py-1 rounded-full cursor-pointer">
+          Get Started
+        </h3>
       </div>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Header from '../components/Header';
 import { sanityClient, urlFor } from '../sanity';
 import { Post } from '../typings';
@@ -21,16 +22,44 @@ export default function Home({ posts }: Props) {
             <span className="underline decoration-black decoration-4">Small</span> is a place to
             write, read, and connect
           </h1>
-          <h2 className="font-semibold">
-            It's easy and free to post you thinking on any topic and connect with millions of
+          <h2 className="">
+            It's easy and free to post your thinking on any topic and connect with millions of
             readers.
           </h2>
+          <h3 className="bg-white w-32 border border-black px-4 py-2 text-center rounded-full cursor-pointer">
+            Start Writing
+          </h3>
         </div>
         <img
           className="hidden md:inline-flex h-48 py-10 px-14 lg:h-full"
           src="/SLogo.png"
           alt="S logo"
         />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="border rounded-md group cursor-pointer overflow-hidden">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt="post image"
+              />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p>{post.description}</p>
+                  <p className="text-xs text-gray-500">{post.author.name}</p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt="author avatar"
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
