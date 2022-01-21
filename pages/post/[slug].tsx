42df6cf03ba0cc7321f22c9638b77d2d1e2c93a7
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2022-01-21 16:54:57 -0600

    added

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,10 @@
+import Header from '../../components/Header';
+import { sanityClient, urlFor } from '../../sanity';
+
+const Post = () => {
+  return <div></div>;
+};
+
+export default Post;
+
+export const getStaticProps = async () => {};
