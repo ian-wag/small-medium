4f1ffe1743a49a69d8ff6d5ea61c48e168b94a5f
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2022-02-02 15:07:01 -0600

    comment schema for sanity studio

---
+++ smallmedium/schemas/comment.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'comment',
+  type: 'document',
+  title: 'Comment',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+    },
+    {
+      title: 'Approved',
+      name: 'approved',
+      type: 'boolean',
+      description: 'Comments only show once approved',
+    },
+    {
+      name: 'email',
+      type: 'string',
+    },
+    {
+      name: 'comment',
+      type: 'text',
+    },
+    {
+      name: 'post',
+      type: 'reference',
+      to: [{ type: 'post' }],
+    },
+  ],
+};
