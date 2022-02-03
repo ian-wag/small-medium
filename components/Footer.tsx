29ee0c689fdb24e618446000418a710f9c313cc6
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2022-02-03 17:19:55 -0600

    added

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className="text-center p-10 mt-10 border-t border-gray-200 text-gray-400">
+      <a
+        className="hover:text-gray-500"
+        href="https://github.com/ian-wag/small-medium"
+        target="_blank"
+        rel="noreferrer"
+      >
+        MADE BY IAN 2022
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
